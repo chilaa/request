29def2ba04ec7b0e16c1d5894756bd9f6968aa7f
chilaa <farnnn.ff@gmail.com>
2019-01-17 23:53:45 +0400

    done all

--- Request.php
+++ Request.php
@@ -5,7 +5,7 @@ function getHeaders(){
     foreach ($_SERVER as $keyName => $keyValue){
         if (strpos($keyName, 'HTTP_')!==false){
 
-            print_r($keyName . "=>" . $keyValue . "<br>");
+            print_r($keyName . "=>" . $keyValue . "\n");
         }
     }
 }
@@ -13,33 +13,41 @@ function getPath(){
     if (isset($_SERVER['PATH_INFO'])){
         print_r($_SERVER['PATH_INFO']);
     }
-    else print_r('No path is set');
+    else return ('No path is set');
 }
 function getScheme(){
-    
+    return $_SERVER['SERVER_PROTOCOL'];
 }
 function getPort(){
-
+    $port=$_SERVER['SERVER_PORT'];
+    return $port;
 }
 function getQueryParameters(){
-
+    $query=$_GET;
+    return $query;
 }
 function get($key){
-
+    return $_SERVER[$key];
 }
 function getData(){
-
+    return $_POST;
 }
-function fetClientIp(){
-
+function getClientIp(){
+    return $_SERVER['REMOTE_ADDR'];
 }
 function getCountry(){
-
-
+    $coutry=unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
+    return $coutry['geoplugin_countryName'];
 }
-function getMineType(){
-
+function getMimeType(){
+    $format=explode('/', $_SERVER['HTTP_ACCEPT']);
+    return $format[1];
 }
 function getBaseUrl(){
-
+    $name=$_SERVER['SERVER_NAME'];
+    if (getScheme()=='http'){
+        return "http://$name";
+    }else if (getScheme()=='https'){
+        return "https://$name";
+    }
 }
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 require_once 'Request.php';
 
-//print_r($_SERVER);
+print_r($_SERVER);
 getHeaders();
-getPath();
+print_r(getPath());
+print_r(getQueryParameters());
+print_r(get('PATH_INFO'));
+echo 'hey';
+print_r(getMimeType());
+print_r(getCountry());
 //print_r(getallheaders());
